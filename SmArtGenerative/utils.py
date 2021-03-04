8c0491ebf84a15dd0ccc2aa3b32c6a7ac5a86370
Jae Kim <byungjae91@gmail.com>
2021-03-04 10:47:57 +0000

    Added scripts for lbgfs style transfer class

---
+++ SmArtGenerative/utils.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+import torch
+import torchvision.transforms as T
+
+
+def loader(path):
+'''Convert PIL image into torch tensor with dummy dimension'''
+  preproc = T.Compose([T.ToTensor()])
+  img = Image.open(path)
+  img_proc = preproc(img).unsqueeze(0)
+  return img_proc
+
+def unloader(img):
+'''convert torch tensor into PIL image'''
+  t_unload = T.ToPILImage()
+  img = t_unload(img)
+  return img
+
+def gram_matrix(input_tensor):
+    # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
+    a, b, c, d = input_tensor.size()
+    features = input_tensor.view(a * b, c * d)
+    # calculate gram matrix
+    G = torch.mm(features, features.t())
+    # normalise
+    return G.div(a * b * c * d)
