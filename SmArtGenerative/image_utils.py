a4bbb21ce162524e6207639487e5f5937caba204
Omer Aziz <omeraziz10@gmail.com>
2021-03-07 20:43:21 +0000

    streamlit style gallery interface + example slideshow

--- SmArtGenerative/image_utils.py
+++ SmArtGenerative/image_utils.py
@@ -4,6 +4,38 @@ import PIL.Image
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
+from google.cloud import storage
+import requests
+from io import BytesIO
+
+BUCKET_NAME = 'smartgenerative'
+
+BUCKET_DATA_PATH = 'style/style/'
+
+BASE_URI = 'https://storage.googleapis.com/smartgenerative/'
+
+STYLES = {0:'Alley by the Lake - Leonid Afremov',
+              1: 'Young Waterbearer - Adam Styka',
+              2: 'Sleep Till Spring - David Michael Hinnebusch',
+              3: 'Sunday Afternoon on the Island of La Grande Jatte - Georges Seurat',
+              4: 'Beaute Calme et Volupte - Houria Niati',
+              5: 'Hands Flowers Eyes',
+              6: 'Murnau Street with Women - Wassily Kandinsky',
+              7: 'Political Convergence - Jackson Pollock',
+              8: 'Mona Lisa - Leonardo da Vinci',
+              9: 'The Scream - Edvard Munch',
+              10: 'Creation of Adam - Michelangelo',
+              11: 'Wheatfield with Cypress Tree - Vincent van Gogh',
+              12: 'The Starry Night - Vincent van Gogh',
+              13: 'The Great Wave off Kanagawa - Katsushika Hokusai',
+              14: 'The Last Supper - Leonardo da Vinci',
+              15: 'The Fighting Temeraire - J.M.W. Turner',
+              16: 'The Son of Man - Rene Magritte',
+              17: 'Nighthawks - Edward Hopper',
+              18: 'The New Abnormal - The Stokes',
+              19: 'The Garden - Joan Miro',
+              20: 'Painting (1933) - Joan Miro'
+              }
 
 def tensor_to_image(tensor):
   '''Converts output tensor back to an image'''
@@ -31,9 +63,12 @@ def load_img(path_to_img):
   img = img[tf.newaxis, :]
   return img
 
-def load_uploaded_image(image):
+def load_uploaded_image(image, style=False):
   max_dim = 512
-  img = PIL.Image.open(image)
+  if style:
+    img = image
+  else:
+    img = PIL.Image.open(image)
   img = tf.convert_to_tensor(img_to_array(img)/255)
 
   shape = tf.cast(tf.shape(img)[:-1], tf.float32)
@@ -45,6 +80,19 @@ def load_uploaded_image(image):
   img = tf.image.resize(img, new_shape)
   img = img[tf.newaxis, :]
   return img
+
+def load_styles():
+  images = []
+  client = storage.Client()
+  bucket = client.bucket(BUCKET_NAME)
+  blobs = bucket.list_blobs(prefix=BUCKET_DATA_PATH)
+
+  for blob in blobs:
+    url = BASE_URI+blob.name
+    response = requests.get(url)
+    img = PIL.Image.open(BytesIO(response.content))
+    images.append(img)
+  return images
 
 def imshow(image, title=None):
   if len(image.shape) > 3:
